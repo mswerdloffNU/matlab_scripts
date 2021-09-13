089597859ba6e3c4ad6073bc75452d3b8f87c9eb
Margaret Swerdloff <margaretswerdloff2016@u.northwestern.edu>
2021-09-13 10:50:43 -0500

    added a check to make sure there are no other targets that could have been included in either S1 or S2

--- accrej_compare.m
+++ accrej_compare.m
@@ -32,14 +32,22 @@ for ii = 1:size(codesAcceptedS1,1)
     for jj = 1:size(codesAcceptedS1,2)
         if codesAcceptedS1{ii,jj} == 1 && codesAcceptedS2{ii,jj} == 1
             codesCompare(ii,jj) = 1;
+        elseif codesAcceptedS1{ii,jj} == 1 && codesAcceptedS2{ii,jj} ~= 1
+            codesCompare(ii,jj) = 9;
+        elseif codesAcceptedS1{ii,jj} ~= 1 && codesAcceptedS2{ii,jj} == 1
+            codesCompare(ii,jj) = 99;
         end
     end
 end
 
-% apply check37
+check9 = numel(find(codesCompare(:)==9)); % count any other targets in S1
+check99 = numel(find(codesCompare(:)==99)); % count any other targets in S2
+
+% apply check37, check9, and check99
 check37 = zeros(1,3);
 check37 = [numel(find(codesCompare(:,1)==1)) numel(find(codesCompare(:,2)==1)) numel(find(codesCompare(:,3)==1))];
-if check37(1) == 37 && check37(2) == 37 && check37(3) == 37
+
+if check37(1) == 37 && check37(2) == 37 && check37(3) == 37 && check9 == 0 && check99 == 0
     sprintf('we good!')
 else
     pause
